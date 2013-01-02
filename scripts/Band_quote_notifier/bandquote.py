d47b39a36d6668685df99fff8ff4f62ed8e5a05e
Todd Wiltz <t.miester100@gmail.com>
2013-01-02 13:16:34 -0600

    Added another program to the list.

---
+++ scripts/Band_quote_notifier/bandquote.py
@@ -0,0 +1,8 @@
+import random
+
+# has a collection of your favorite song/band quotes and prints them 
+# edit your own quotes to personalize, these are just examples
+# If you're wondering where the quotes came from it's Machinae Supremacy - Radio Future, Machinae Supremacy - Laser Speed Force, Machinae Supremacy - Nova Prospekt, Machinae Supremacy - Cybergenesis, Machinae Supremacy - Through The Looking Glass, and Machinae Supremacy - Reanimator.
+quote_list = ["I wanna see the underground rise up and soar!", "Her song leaves the other side in flight for their lives now, they are not long for this world, No one leaves, line up for inevitable wipeout, all you've done now it's what you deserve!", "Time for the final blow Last stand, now go go go The time for running now is over You will reap what you sow Payback for what you owe No more, we won't just hand it over!", "There's no fate but that which we make for ourselves But we are a breath, just a moment in time and space", "Come with me and you will see, our future in debris, first the sun and now the stars are fading", "We'll walk a thousand miles just to see for ourselves, Look behind our eyes, find our hell, And in the light of the sun we go Through rain and raging snow to find the things we do not know", "The world belongs to those, who stand out from the rows, of wayward and misguided silent souls, History is shamed By people laying blame To everyone who doesn't feel the same"]
+quote_choice = random.choice (quote_list)
+print (quote_choice)
