f667d0db95582f8b7f6dabb92e523ea719ec1973
Todd Wiltz <t.miester100@gmail.com>
2013-01-02 13:42:50 -0600

    Added a notification version of the magic8ball.

---
+++ scripts/magic8ball/Magic8ball_notifier/magic8ball.py
@@ -0,0 +1,5 @@
+import random
+
+eightBall_list = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely!", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook is good.", "Yes!", "All signs point to yes.", "Reply hazy, try again.", "Ask again later.", "I can't tell you now.", "Can't predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook is not so good.", "Very doubtful."]
+eightBall_choice = random.choice (eightBall_list)
+print (eightBall_choice)
